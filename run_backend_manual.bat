f848f0feda41d278aff6d0812c06aea5da4696cd
surajsingh8204 <surajsingh8204@gmail.com>
2025-12-17 16:44:55 +0530

    Remove unnecessary BAT files for Render deployment

--- run_backend_manual.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Activating conda environment from S:\mldeeplearningai...
-call S:\mldeeplearningai\Scripts\activate.bat
-
-cd /d %~dp0backend
-
-echo Starting FastAPI server...
-python main.py
